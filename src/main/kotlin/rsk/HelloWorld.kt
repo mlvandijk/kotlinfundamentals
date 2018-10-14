51751f36598aeea9d88666d6ded8eafc0f364048
maritvandijk <mlvandijk@gmail.com>
2018-10-14 10:51:32 +0200

    Named parameters

--- src/main/kotlin/rsk/HelloWorld.kt
+++ src/main/kotlin/rsk/HelloWorld.kt
@@ -5,6 +5,7 @@ fun main(args: Array<String>) {
     display("Hello, World")
     log("Hello, World") // Using default
     log("Hello, World", 2) // Overriding the default
+    log(logLevel = 2, message = "Order doesn't matter with named parameters")
 }
 
 fun display(message: String) {
