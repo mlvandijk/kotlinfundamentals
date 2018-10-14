57acec2a775818dea3af5416695b72e90689bf48
maritvandijk <mlvandijk@gmail.com>
2018-10-14 10:43:42 +0200

    Calling function from Java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,7 +2,9 @@ import rsk.*;
 
 public class App {
 
+    // Calling a Kotlin function from Java, using the `@file: JVMName` annotation:
     public static void main(String[] args) {
-        DisplayFunctions.display("Hello, World - from Java");
+        DisplayFunctions.display("Hello, World from Java");
+        DisplayFunctions.log("Hello, World from Java", 3);
     }
 }
--- src/main/kotlin/rsk/HelloWorld.kt
+++ src/main/kotlin/rsk/HelloWorld.kt
@@ -3,8 +3,15 @@ package rsk
 
 fun main(args: Array<String>) {
     display("Hello, World")
+    log("Hello, World") // Using default
+    log("Hello, World", 2) // Overriding the default
 }
 
 fun display(message: String) {
     println(message)
+}
+
+// Using default parameters:
+fun log(message: String, logLevel: Int = 1) {
+    println(message + logLevel)
 }
