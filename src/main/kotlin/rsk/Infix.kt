7540d0be430d648c4bfcee0038ee37d7619d70c7
maritvandijk <mlvandijk@gmail.com>
2018-10-14 11:24:16 +0200

    infix function

--- src/main/kotlin/rsk/Infix.kt
+++ src/main/kotlin/rsk/Infix.kt
@@ -5,7 +5,7 @@ fun main(args: Array<String>) {
     val h1 = Header("H1")
     val h2 = Header("H2")
 
-    val h3 = h1.plus(h2)
+    val h3 = h1 plus h2
 
     println(h3.name)
 
@@ -15,6 +15,6 @@ class Header(var name: String) {
 
 }
 
-fun Header.plus(other: Header) : Header {
+infix fun Header.plus(other: Header) : Header {
     return Header(this.name + other.name)
 }
