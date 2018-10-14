2c79dadc679897cd15bc3e0b580dc0b5ec8bfa63
maritvandijk <mlvandijk@gmail.com>
2018-10-14 11:42:01 +0200

    tailrec function

--- src/main/kotlin/rsk/Tailrec.kt
+++ src/main/kotlin/rsk/Tailrec.kt
@@ -3,11 +3,11 @@ package rsk
 import java.math.BigInteger
 
 fun main(args: Array<String>){
-    println(fib(10, BigInteger("1"), BigInteger("0"))) // 55
+    println(fib(100000, BigInteger("1"), BigInteger("0"))) // 55
 }
 
-// Recursive function
-fun fib(n: Int, a: BigInteger, b: BigInteger) :BigInteger {
+// tailrec function; Kotlin will replace with a loop in the bytecode to prevent stack overflow
+tailrec fun fib(n: Int, a: BigInteger, b: BigInteger) :BigInteger {
     return if (n == 0) b else fib (n -1, a + b, a)
 }
 
