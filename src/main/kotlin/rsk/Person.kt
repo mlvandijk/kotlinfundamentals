fae8f1f7c01803dc72788a85ff49321f41dec6f8
Marit van Dijk <maritvandijk@bol.com>
2018-10-15 15:43:22 +0200

    Add secondary constructor

--- src/main/kotlin/rsk/Person.kt
+++ src/main/kotlin/rsk/Person.kt
@@ -2,24 +2,30 @@ package rsk
 
 
 fun main(args: Array<String>) {
-    val kevin = Student(1)
+    val kevin = Student("Kevin", "Jones", 1)
     print(kevin.id)
 }
 
-abstract class Person {
-    var firstName: String = ""
-    var lastName: String = ""
+abstract class Person(var firstName: String, var lastName: String) {
 
     open fun getName() : String = "$firstName $lastName"
     abstract fun getAddress():String
 }
 
-class Student(val id: Int) : Person() {
+class Student(firstName: String, lastName: String, val id: Int) : Person(firstName, lastName) {
+    var tutor: String = ""
+
+    constructor(firstName: String, lastName: String, id: Int, tutor:String): this(firstName, lastName, id) {
+        this.tutor = tutor
+    }
+
     override fun getName() : String{return ""}
 
     // Abstract method must be implemented (or this class should also be declared abstract
     override fun getAddress(): String {
         return ""
     }
+
+
 }
 
